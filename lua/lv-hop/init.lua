4685ac6a0ec8a12a073deb83103adf57a59754ab
erix <erix.andersson@gmail.com>
2021-07-20 12:43:17 +0200

    minor

--- lua/lv-hop/init.lua
+++ lua/lv-hop/init.lua
@@ -7,6 +7,6 @@
 -- sadfjklewcmpgh
 require'hop'.setup { keys = 'asdghklqwertyuiopzxcvbnmfj', term_seq_bias = 0.5 }
 
-vim.api.nvim_set_keymap('', 's', ":HopChar1<cr>", {silent = true})
+vim.api.nvim_set_keymap('', 's', ":HopChar2<cr>", {silent = true})
 vim.api.nvim_set_keymap('', 'S', ":HopPattern<cr>", {silent = true})
 
